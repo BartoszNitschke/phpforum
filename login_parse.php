c18a42e70117f2e7e4d329127ee5fb329c0ee8ea
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2022-06-16 14:44:15 +0200

    Create login_parse.php
    file responsible for log in action

---
+++ login_parse.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+include_once ("connection.php");
+
+if(isset($_POST['username'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = " SELECT * FROM users WHERE username='".$username."' AND password='".$password."' LIMIT 1";
+    $res = mysqli_query($link,$sql);
+
+    if(mysqli_num_rows($res) == 1){
+        $row = mysqli_fetch_assoc($res);
+        $_SESSION['uid'] = $row['id'];
+        $_SESSION['username'] =  $row['username'];
+        $_SESSION['role'] = $row['role'];
+        header("Location: index.php");
+        exit();
+    }else{
+        echo "Niepoprawny login lub hasło, powróć do strony głównej i spróbuj ponownie";
+        exit();
+    }
+}
