cba72b3c6d1d7bee0ce6a8c37daf6a82be669c15
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2022-06-23 14:56:01 +0200

    script for deleting account

---
+++ phpfiles/delete_account.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if($_SESSION['uid']){
+    if(isset($_POST['delete_account'])){
+        include_once ("connection.php");
+        $id = $_SESSION['uid'];
+
+
+
+            $sql = "DELETE FROM users WHERE id='".$id."'";
+            $res = mysqli_query($link, $sql) or die(mysqli_error());
+
+            if (($res)) {
+                session_destroy();
+                echo "<p>Twoje konto zostało usunięte. <a href='index.php'>Kliknij, aby wrócić na stronę główną</a></p>";
+            } else {
+                echo "<p>Wystąpił problem ze zmianą loginu.Spróbuj ponownie</p></br><a href='settings.php'>Kliknij by wrócić do ustawień</a>";
+            }
+
+    }else{
+        exit();
+    }
+}else{
+    header("Location: index.php");
+    exit();
+}
+?>
