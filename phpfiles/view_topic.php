caae9329197fbd892fa0d1b96e52a266c92ee193
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2022-06-24 20:31:40 +0200

    view topic page

--- phpfiles/view_topic.php
+++ phpfiles/view_topic.php
@@ -7,9 +7,9 @@
     <Title>Forum</Title>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-    crossorigin="anonymous">
+          rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+          crossorigin="anonymous">
     <link rel="stylesheet"  href="style.css"/>
 
 </head>
@@ -47,134 +47,126 @@
 
     <hr/>
     <div class="content">
-       <?php
-       $tid="";
-       include_once ("connection.php");
-       $cid = $_GET['cid'];
-       $tid = $_GET['tid'];
-       $sql = "SELECT * FROM topics WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
-       $res = mysqli_query($link,$sql) or die(mysqli_error());
-       if(isset($_SESSION['uid']) && ($_SESSION['role'] == 1 || $_SESSION['role'] == 2)) {
-
-               echo "<div style='padding-bottom: 10px;'><form action='delete_post.php' method='post'>
+        <?php
+        $tid="";
+        include_once ("connection.php");
+        $cid = $_GET['cid'];
+        $tid = $_GET['tid'];
+        $sql = "SELECT * FROM topics WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
+        $res = mysqli_query($link,$sql) or die(mysqli_error());
+        if(isset($_SESSION['uid'])) {
+            if ($_SESSION['role'] == 1 || $_SESSION['role'] == 2) {
+                echo "<div style='padding-bottom: 10px;'><form action='delete_post.php' method='post'>
             <input type='hidden' name='top_id' value='" . $tid . "'>
             <input type='hidden' name='cat_id' value='" . $cid . "'>
             <input type='submit' name='delete' value='Usuń cały wątek' class='btn btn-light'></form></div>
-
 ";
 
 
+            }
+        }
 
-       }
 
-
-       if(mysqli_num_rows($res) == 1){
-           echo "<table width='100%'>";
-           while($row = mysqli_fetch_assoc($res)){
-               $sql2 = "SELECT * FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."'";
-               $res2 = mysqli_query($link,$sql2) or die(mysqli_error());
-               $i=1;
-               while($row2 = mysqli_fetch_assoc($res2)){
-                   echo "<tr><td valign='top' style='border: 1px solid #000000; background-color: #cccccc;'><div style='min-height: 125px;'><b>Temat: ".$row['topic_title']."</b><br/>
+        if(mysqli_num_rows($res) == 1){
+            echo "<table width='100%'>";
+            while($row = mysqli_fetch_assoc($res)){
+                $sql2 = "SELECT * FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."'";
+                $res2 = mysqli_query($link,$sql2) or die(mysqli_error());
+                $i=1;
+                while($row2 = mysqli_fetch_assoc($res2)){
+                    echo "<tr><td valign='top' style='border: 1px solid #000000; background-color: #cccccc;'><div style='min-height: 125px;'><b>Temat: ".$row['topic_title']."</b><br/>
                    by <b>".$row2['post_creator']."</b> - ".$row2['post_date']."<hr/>".$row2['post_content']."</div></td></tr>
                    <tr><td colspan='2'>";
-                   if(isset($_SESSION['uid']) && ($_SESSION['role'] == 1)) {
+                    if(isset($_SESSION['uid'])) {
 
-                           if ($i != 1) {
-                               echo "<div style='display: flex;'><div style='padding-right: 10px;'><form action='delete_comment.php' method='post'>
+                        
+                        if ($_SESSION['role'] == 1) {
+                            if ($i != 1) {
+                                echo "<div style='display: flex;'><div style='padding-right: 10px;'><form action='delete_comment.php' method='post'>
                             <input type='hidden' name='top_id' value='" . $row2['topic_id'] . "'>
                             <input type='hidden' name='cat_id' value='" . $row2['category_id'] . "'>
                             <input type='hidden' name='post_id' value='" . $row2['id'] . "'>
                            <input type='submit' name='delete_comment_button' value='Usuń zawartość' class='btn btn-light'></div>
-
 </form>";
 
 
-                           }
+                            }
 
-                           echo "<form action='edit_topic.php' method='post'>
+                            echo "<form action='edit_topic.php' method='post'>
                             <input type='hidden' name='top_id' value='" . $row2['topic_id'] . "'>
                             <input type='hidden' name='cat_id' value='" . $row2['category_id'] . "'>
                             <input type='hidden' name='post_id' value='" . $row2['id'] . "'>
                             <input type='hidden' name='post_content' value='" . $row2['post_content'] . "'>
                            <input type='submit' name='edit_topic_button' value='Edytuj zawartość' class='btn btn-light'>
-
 </form></div>";
-                       }
+                        }
 
 
 
-                        if(isset($_SESSION['uid']) && ($_SESSION['role'] == 2)) {
-                           if ($i != 1) {
-                               echo "<form action='delete_comment.php' method='post'>
+                        if ($_SESSION['role'] == 2) {
+                            if ($i != 1) {
+                                echo "<form action='delete_comment.php' method='post'>
                             <input type='hidden' name='top_id' value='" . $row2['topic_id'] . "'>
                             <input type='hidden' name='cat_id' value='" . $row2['category_id'] . "'>
                             <input type='hidden' name='post_id' value='" . $row2['id'] . "'>
                            <input type='submit' name='delete_comment_button' value='Usuń zawartość' class='btn btn-light'>
-
 </form>";
 
 
-                           }
-                       }
+                            }
+                        }
 
-                       if ($_SESSION['role'] == 0 && $row2['post_creator'] == $_SESSION['username']) {
+                        if ($_SESSION['role'] == 0 && $row2['post_creator'] == $_SESSION['username']) {
 
 
-                           if ($i == 1) {
-                               echo "<form action='delete_post.php' method='post'>
+                            if ($i == 1) {
+                                echo "<form action='delete_post.php' method='post'>
             <input type='hidden' name='top_id' value='" . $tid . "'>
             <input type='hidden' name='cat_id' value='" . $cid . "'>
             <input type='submit' name='delete' value='Usuń cały wątek' class='btn btn-light'></form>
-
 ";
-                           } else {
-                               echo "<form action='delete_comment.php' method='post'>
+                            } else {
+                                echo "<form action='delete_comment.php' method='post'>
                             <input type='hidden' name='top_id' value='" . $row2['topic_id'] . "'>
                             <input type='hidden' name='cat_id' value='" . $row2['category_id'] . "'>
                             <input type='hidden' name='post_id' value='" . $row2['id'] . "'>
                            <input type='submit' name='delete_comment_button' value='Usuń zawartość' class='btn btn-light'>
-
 </form>";
-                           }
-                           echo "<form action='edit_topic.php' method='post'>
+                            }
+                            echo "<form action='edit_topic.php' method='post'>
                             <input type='hidden' name='top_id' value='" . $row2['topic_id'] . "'>
                             <input type='hidden' name='cat_id' value='" . $row2['category_id'] . "'>
                             <input type='hidden' name='post_id' value='" . $row2['id'] . "'>
                             <input type='hidden' name='post_content' value='" . $row2['post_content'] . "'>
                            <input type='submit' name='edit_topic_button' value='Edytuj zawartość' class='btn btn-light'>
-
 </form>";
 
 
-                       }
-                   $old_views = $row['topic_views'];
-                   $new_views = $old_views + 1;
-                   $sql3 = "UPDATE topics SET topic_views='".$new_views."' WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
-                   $res3 = mysqli_query($link, $sql3) or die(mysqli_error());
+                        }
+                    }
 
-                   }
-
-                   $i++;
-                   echo "<hr/></td></tr>";
+                    $i++;
+                    echo "<hr/></td></tr>";
 
 
-               }
+                }
+                $old_views = $row['topic_views'];
+                $new_views = $old_views + 1;
+                $sql3 = "UPDATE topics SET topic_views='".$new_views."' WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
+                $res3 = mysqli_query($link, $sql3) or die(mysqli_error());
+            }
+            echo "</table>";
+            if(isset($_SESSION['uid'])){
+                echo "<tr><td colspan='2'><input class='btn btn-light' type='submit' value='Skomentuj' onClick=\"window.location = 'post_reply.php?cid=".$cid."&tid=".$tid."'\"/><hr/>";
+            }
+            else{
+                echo "<tr><td colspan='2'><p style='color: white; font-size: 22px;'>Zaloguj się, aby dodać odpowiedź</p></td></tr>";
+            }
+        }else{
+            echo"<p>This topic does not exist. </p>";
+        }
 
-           }
-       else{
-           echo"<p>This topic does not exist. </p>";
-       }
-           echo "</table>";
-           if(isset($_SESSION['uid'])){
-               echo "<tr><td colspan='2'><input class='btn btn-light' type='submit' value='Skomentuj' onClick=\"window.location = 'post_reply.php?cid=".$cid."&tid=".$tid."'\"/><hr/>";
-           }
-           else{
-               echo "<tr><td colspan='2'><p style='color: white; font-size: 22px;'>Zaloguj się, aby dodać odpowiedź</p></td></tr>";
-           }
-
-
-       ?>
+        ?>
 
     </div>
 </div>
@@ -190,4 +182,3 @@
 </body>
 
 </html>
-
