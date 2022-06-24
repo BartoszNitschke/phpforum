e93737345d48c0e46b1b4910a34de963d168935e
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2022-06-24 19:43:37 +0200

    script for editing topics - code style updated

--- phpfiles/edit_topic_parse.php
+++ phpfiles/edit_topic_parse.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if($_SESSION['uid']){
-    if(isset($_POST['edit_content'])){
+if($_SESSION['uid'] && isset($_POST['edit_content'])){
+    
         include_once ("connection.php");
         $cid = $_POST['cat_id'];
         $tid = $_POST['top_id'];
@@ -58,9 +58,7 @@ if($_SESSION['uid']){
             echo "<p>Wystąpił problem z usunięciem wątku.Spróbuj ponownie</p></br><a href='view_topic.php?cid=".$obj->getCatId()."&tid=".$obj->getTopId()."'>Kliknij by wrócić do posta</a>";
         }
 
-    }else{
-        exit();
-    }
+ 
 }else{
     header("Location: index.php");
     exit();
